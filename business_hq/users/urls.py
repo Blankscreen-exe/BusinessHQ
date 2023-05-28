1c12f8383ed83d316f0cbad767e95fee4443fd74
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2023-05-28 13:04:21 +0500

    removed unused views for login

--- business_hq/users/urls.py
+++ business_hq/users/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 
 
-from users.views import RegisterView, LoginView
-
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='customer-register'),
-    path('login/', LoginView.as_view(), name='customer-login'),
+
 ]
--- business_hq/users/views.py
+++ business_hq/users/views.py
@@ -6,33 +6,3 @@ from rest_framework.exceptions import AuthenticationFailed
 from users.models import User
 from users.serializers import UserSerializer
 
-class RegisterView(APIView):
-    """Registers users
-    """
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        
-        return Response(serializer.data)
-
-
-class LoginView(APIView):
-    """Login for customers
-    """
-    def post(self, request):
-        email = request.data['email']
-        password = request.data['password']
-        user = User.objects.filter(email=email).first()
-        
-        if user is None:
-            # TODO: change error message
-            raise AuthenticationFailed('User Not Found dumbass')
-        
-        if not user.check_password(password):
-            # TODO: change error message
-            raise AuthenticationFailed('Password not correct BHAAAGA')
-        
-        return Response(user)
-             
-        
