0b4c73b7c492b0bf6c9239f8f06e9021afc195ed
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2023-05-29 00:22:16 +0500

    added route constants

---
+++ front_end/src/routes/main.jsx
@@ -0,0 +1,17 @@
+export const route_links = {
+    // general links
+    "home": "/",
+    "about_us": "/about",
+    "portfolio": "/portfolio",
+    "contact": "/contact",
+
+    // registration and login
+    "login_customer": "/login",
+    "login_staff": "/login/staff",
+    "register_customer": "/register",
+
+    // services
+    "services_catalogue": "/services",
+    "service_details": "/services/:id",
+}
+
