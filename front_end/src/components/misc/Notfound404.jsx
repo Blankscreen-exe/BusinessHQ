9b676d90dc27afb3df66ed28b0e350b6835fa90e
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2023-05-30 22:43:05 +0500

    added 404 page with assets

--- front_end/src/components/misc/Notfound404.jsx
+++ front_end/src/components/misc/Notfound404.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Notfound() {
   return (
-    <h1 align="center"> 404 Not found </h1>
+    <>
+      <img src="/notfound/notfound.jpg" className='not-found-404'/>
+      <h1 align="center" className='not-found-title blue'> 404 Not found </h1>
+      <p className='not-found-message gray'>Sorry about that .. but looks like the page you were looking for does not exist.</p>
+    </>
   )
 }
 
